08277d493b2287a8a804da1143d019943409ad7a
Thomas <mike2k@mail.ru>
2020-08-07 17:33:56 +0600

    Update wlmail.php

--- wp-content/themes/twentytwenty/wlmail.php
+++ wp-content/themes/twentytwenty/wlmail.php
@@ -1,4 +1,20 @@
-<?php
+<?
+if (isset($_POST['id'])) {$id = $_POST['id']; }
+if (isset($_POST['emails'])) {$emails = $_POST['emails'];}
+if (isset($_POST['confirmation'])) {$confirmation = $_POST['confirmation'];}
+if (isset($_POST['sender_email'])) {$sender_email = $_POST['sender_email'];}
+
+$wlfields = 'id='.$id.'&emails='.$emails.'&confirmation='.$confirmation.'&sender_email='.$sender_email;
 
 
-?>
+  if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_URL, 'https://api.sendpulse.com/addressbooks/');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $wlfields3);
+    $out = curl_exec($curl);
+    curl_close($curl);
+  }
+  
+  echo 'ok';
+ ?>
